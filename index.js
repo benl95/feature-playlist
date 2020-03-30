881cd802c481c9cb0e2630b311358c00d65b0283
benl95 <beno_1995@hotmail.com>
2020-03-30 19:56:59 +0200

    setup server, templating engine and packages

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const hbs = require('handlebars')
+const path = require('path')
+const bodyParser = require('body-parser')
+const camelCase = require('camelcase')
+const app = express()
+
+// Port
+app.listen(8080, () => {
+    console.log('Server is starting on port', 8080)
+})
+
+// Static folders 
+app.use(express.static(path.join(__dirname, '/public')))
+
+// Hbs setup 
+app.engine('handlebars', expbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
+app.set('view engine', 'handlebars')
